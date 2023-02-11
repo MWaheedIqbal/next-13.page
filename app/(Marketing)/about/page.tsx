3b2a2a0c84f70c3dd60fdd49326cf2f7ff4e6ccb
M. Waheed Iqbal <tesla.dxb@gmail.com>
2023-02-11 23:51:36 +0400

    created next13-page

---
+++ app/(Marketing)/about/page.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+import Image from "next/image";
+
+export default function Home() {
+    return (
+        <>
+            <h1>About</h1>
+            <Link href={"/"}>Home</Link>
+            <Image src="next.svg"
+                alt="About image"
+                width={"100"}
+                height="100" />
+        </>
+    );
+}
